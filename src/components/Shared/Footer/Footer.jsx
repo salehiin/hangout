8609a582b6204e7ecf469f93f5af56f78c66c94d
Shoeb Salehin Khan <salehin.ay@gmail.com>
2024-06-10 03:07:16 +0600

    footer done

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,8 +1,18 @@
+import { FaSquareFacebook, FaSquareTwitter, FaInstagram, FaSquareWhatsapp } from "react-icons/fa6";
+
+
 const Footer = () => {
   return (
-    <footer className='px-4 divide-y  text-gray-800 relative bottom-0 left-0 bg-blue-300'>
-      <div className='py-6 text-sm text-center text-black'>
+    <footer className='px-10 flex items-center justify-between  text-gray-800 relative bottom-0 left-0 bg-black'>
+      <div className='py-6 text-sm text-center text-blue-300'>
         © 2024-2025 HangOut Inc. All rights reserved.
+      </div>
+      <div className="text-yellow-500 flex gap-6 text-2xl">
+      <FaSquareFacebook />
+      <FaSquareTwitter />
+      <FaInstagram />
+      <FaSquareWhatsapp />
+
       </div>
     </footer>
   )
